1431988ac3caac912b1b9ed12494b093603231cc
Sam Lendle <sam.lendle@gmail.com>
2014-03-01 11:26:59 -0800

    Test for NaNs in result

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,3 +12,5 @@ resx, resy= locpoly(x, y, 0.25)
 
 @test_approx_eq_eps(maximum(abs(resx - expected[:,1])), 0.0, sqrt(eps(Float64)))
 @test_approx_eq_eps(maximum(abs(resy - expected[:,2])), 0.0, sqrt(eps(Float64)))
+
+@test !any(isnan(resy))
